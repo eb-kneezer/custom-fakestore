083abb3d8d048e0d8d7a3b927c5f5929569d642a
Ebenezer Ogungbe <ogungbesegun2000@gmail.com>
2021-08-13 13:07:41 +0100

    tries to fix cors

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const productsModel = require("./model/products.model");
 
 const app = express();
+const PORT = process.env.PORT || 6969;
+
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 require("./startup/prod")(app);
-
-const PORT = process.env.PORT || 6969;
 
 app.get("/products", (req, res) => {
   res.send(productsModel.getAllProducts());
