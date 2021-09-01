7f3bf8189d0f6e792bc095209ecaf4ed2865ecb2
Ebenezer Ogungbe <ogungbesegun2000@gmail.com>
2021-09-01 18:02:01 +0100

    adds users resource

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const productsModel = require("./model/products.model");
+const usersModel = require("./model/users.model");
 
 const app = express();
 const PORT = process.env.PORT || 6969;
 
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.append("Access-Control-Allow-Origin", ["*"]);
+//   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.append("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -22,6 +26,8 @@ require("./startup/prod")(app);
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// ==========PRODUCTS===============
 
 app.get("/products", (req, res) => {
   const limit = +req.query.limit || 0;
@@ -38,6 +44,21 @@ app.get("/products/category/:category", (req, res) => {
 
 app.get("/products/:id", (req, res) => {
   res.send(productsModel.getProduct(+req.params.id));
+});
+
+// ===========USER=================
+
+app.get("/users", (req, res) => {
+  const limit = +req.query.limit || 0;
+  res.send(usersModel.getAllUsers(limit));
+});
+
+app.get("/users/gender/:gender", (req, res) => {
+  res.send(usersModel.getUserByGender(req.params.gender));
+});
+
+app.get("/users/:id", (req, res) => {
+  res.send(usersModel.getUser(+req.params.id));
 });
 
 app.listen(PORT, () => {
